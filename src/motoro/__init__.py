307541e7aa59eb23c93d43d6f59d36c0093100b7
Bastien Pinel <bastienbpinel@gmail.com>
2023-03-30 09:29:50 +0200

    added camera

--- src/motoro/__init__.py
+++ src/motoro/__init__.py
@@ -4,5 +4,6 @@ from .game_base_object import GameBaseObject
 from .game_base_bloc import GameBaseBloc
 from .game_base_entity import GameBaseEntity
 from .better_controller import BetterController
+from .camera import camera
 
 print('Hello from Noruaric !')
